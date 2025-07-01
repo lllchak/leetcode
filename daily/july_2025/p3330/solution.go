89686cbad6e1bf9b3852a351971d462193ac34b1
Pavel Lyulchak <mediumchak@yandex.ru>
2025-07-01 14:58:02 +0300

    p3330 & p0088 description

--- daily/july_2025/p3330/solution.go
+++ daily/july_2025/p3330/solution.go
@@ -1,5 +1,32 @@
 package p3330
 
+/*
+Alice is attempting to type a specific string on her computer. However, she tends to be clumsy and may
+press a key for too long, resulting in a character being typed multiple times.
+
+Although Alice tried to focus on her typing, she is aware that she may still have done this at most once.
+
+You are given a string word, which represents the final output displayed on Alice's screen.
+
+Return the total number of possible original strings that Alice might have intended to type.
+
+Example 1:
+Input: word = "abbcccc"
+Output: 5
+
+Example 2:
+Input: word = "abcd"
+Output: 1
+
+Example 3:
+Input: word = "aaaa"
+Output: 4
+
+Constraints:
+	- 1 <= word.length <= 100
+	- word consists only of lowercase English letters.
+*/
+
 func possibleStringCount(word string) int {
 	ans := 1
 
