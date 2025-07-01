10c8a0647dbe4d2efeac34407e661aad17101ddd
Pavel Lyulchak <mediumchak@yandex.ru>
2025-07-01 12:45:44 +0300

    p3330

--- daily/july_2025/p3330/solution_test.go
+++ daily/july_2025/p3330/solution_test.go
@@ -8,19 +8,18 @@ import (
 
 func Test(t *testing.T) {
 	input1 := "abbcccc"
-	input2 := "abcd"
-	input3 := "aaaa"
-
 	got1 := possibleStringCount(input1)
 	want1 := 5
 
+	input2 := "abcd"
 	got2 := possibleStringCount(input2)
 	want2 := 1
 
+	input3 := "aaaa"
 	got3 := possibleStringCount(input3)
 	want3 := 4
 
 	assert.Equalf(t, want1, got1, "input = %s", input1)
-	assert.Equal(t, want2, got2, "input = %s", input2)
-	assert.Equal(t, want3, got3, "input = %s", input3)
+	assert.Equalf(t, want2, got2, "input = %s", input2)
+	assert.Equalf(t, want3, got3, "input = %s", input3)
 }
