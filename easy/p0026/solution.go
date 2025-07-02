2f86450af094a7cb51c249db6dbeed725c6abe83
Pavel Lyulchak <mediumchak@yandex.ru>
2025-07-02 11:37:56 +0300

    p0026

--- easy/p0026/solution.go
+++ easy/p0026/solution.go
@@ -20,7 +20,7 @@ Input: nums = [0,0,1,1,1,2,2,3,3,4]
 Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
 
 Constraints:
-	- 1 <= nums.length <= 3 * 104
+	- 1 <= nums.length <= 3 * 10^4
 	- -100 <= nums[i] <= 100
 	- nums is sorted in non-decreasing order.
 */
